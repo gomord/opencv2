a841fa4e4b4c281d69794c2d8e94da3608260d23
moti <gomord@gmail.com>
2015-05-21 17:54:36 +0000

    change avg out of main

---
+++ ctlMotor/motor.h
@@ -0,0 +1,5 @@
+#ifdef _MOTOR_H
+int init_motor(int max_x,int max_y);
+int set_motor(int motor);
+int set_motors(int x,int y);
+#endif //_MOTOR_H
--- fish/fish.cpp
+++ fish/fish.cpp
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <time.h>
-//#include <iostream>
-//#include <fstream>
 #include <cv.h>
 #include "highgui.h"
 #include "mouse.h"
-//using namespace std;
-
-int        g_slider_hue_max= 186;
-int        g_slider_hue_min= 123;
-int        g_slider_sat_max= 193;
-int        g_slider_sat_min= 89;
-int        g_slider_val_max= 249;
-int        g_slider_val_min= 81;
+#define AVG_POS 0
+#define RANGE_POS 1
+int        g_slider_hue_max = 186;
+int        g_slider_hue_min = 123;
+int        g_slider_sat_max = 193;
+int        g_slider_sat_min = 89;
+int        g_slider_val_max = 249;
+int        g_slider_val_min = 81;
+int        g_mouse	    = 0;
 CvScalar   g_hsv_max;
 CvScalar   g_hsv_min;
 CvCapture* g_capture         = NULL;
@@ -36,6 +35,16 @@ void onTrackbarSlide(int pos) {
 	//g_slider_position = pos;
 }
 
+void onTrackbarMouse(int pos){
+	printf("test pos %d, g_mouse %d\n",pos, g_mouse);
+}
+void set_mouse_bar(const char* win_name){
+	cvCreateTrackbar("avg-range",
+			win_name,
+			&g_mouse,
+			1,
+			onTrackbarMouse);
+}
 void set_trac_bar(const char* win_name){
 	cvCreateTrackbar(
 			"Hue Max",
@@ -114,21 +123,43 @@ void set_sliders_filter(const char * win_name, CvScalar *avg,CvScalar *stn){
 }
 clock_t begin, end;
 double	time_spent;
+CvScalar  g_avgScalar;
+CvScalar  g_stnScalar;
+MouseParams g_msPrm = {0};
+int set_avg_pos( IplImage* cl_frame){
+	cvSetImageROI(cl_frame,g_msPrm.box);
+	cvAvgSdv(cl_frame,&g_avgScalar,&g_stnScalar,NULL);
+	set_sliders_filter("set_HSV",&g_avgScalar,&g_stnScalar);
+	printf("avg h - %f s - %f v - %f\n",
+			g_avgScalar.val[0],
+			g_avgScalar.val[1],
+			g_avgScalar.val[2]
+	      );
+	printf("avg hs - %f ss - %f vs - %f\n",
+			g_stnScalar.val[0],
+			g_stnScalar.val[1],
+			g_stnScalar.val[2]
+	      );
+	cvResetImageROI(cl_frame);
+	//cvCvtColor(cl_frame,cl_frame,CV_HSV2RGB);
+	draw_box(cl_frame,g_msPrm.box);
+	cvShowImage("Camera",cl_frame);
+	return 0;
+
+}
 int main( int argc, char** argv ) {
 	
 	int mod = 0;
 	CvMoments mom = {0};
 	CvPoint fishPos = {0,0};
-	MouseParams msPrm = {0};
 	IplImage* frame;
 	IplImage* cl_frame;
 	IplImage* cl_frame_temp;
 	IplImage* gr_frame;
 	CvSize    frameSize ;
-	CvScalar  avgScalar;
-	CvScalar  stnScalar;
 	//cvNamedWindow( "set_HSV", CV_WINDOW_NORMAL);
 	cvNamedWindow( "Camera", CV_WINDOW_NORMAL);
+	set_mouse_bar("Camera");
 	g_capture	= cvCreateCameraCapture( 0 );
 	if(g_capture == NULL){
 		printf("no camra in comp\n");
@@ -142,45 +173,35 @@ int main( int argc, char** argv ) {
 	gr_frame	= cvCreateImage(frameSize,IPL_DEPTH_8U,1);
 //	set_trac_bar("set_HSV");
 	onTrackbarSlide(0);
-	msPrm.isDrawing = false;
-	msPrm.image	= cl_frame;
-	msPrm.box	= cvRect( 0, 0, 1, 1 );
-	mouse("Camera",&msPrm);
+	g_msPrm.isDrawing = false;
+	g_msPrm.image	= cl_frame;
+	g_msPrm.box	= cvRect( 0, 0, 1, 1 );
+	mouse("Camera",&g_msPrm);
 	//printf("hacked frames %d w %d h %d\n",frames,tmpw,tmph);
 
 	begin = clock();
 	while(1) {
 	mod++;
-		if(msPrm.isDrawing){
-			//printf("is dr\n");
-			cvCopy(frame,cl_frame_temp);
-			cvSetImageROI(cl_frame_temp,msPrm.box);
-			cvAvgSdv(cl_frame_temp,&avgScalar,&stnScalar,NULL);
-			set_sliders_filter("set_HSV",&avgScalar,&stnScalar);
-			printf("avg h - %f s - %f v - %f\n",
-			       avgScalar.val[0],
-			       avgScalar.val[1],
-			       avgScalar.val[2]
-			       );
-			printf("avg hs - %f ss - %f vs - %f\n",
-			       stnScalar.val[0],
-			       stnScalar.val[1],
-			       stnScalar.val[2]
-			       );
-			cvResetImageROI(cl_frame_temp);
-			//cvCvtColor(cl_frame_temp,cl_frame_temp,CV_HSV2RGB);
-			draw_box(cl_frame_temp,msPrm.box);
-			if(!(++mod&0x3))
-			cvShowImage("Camera",cl_frame_temp);
+		if(g_msPrm.isDrawing){
+			cvCopy(frame,cl_frame);
+		switch(g_mouse){
+		case RANGE_POS:
+		break;
+		case AVG_POS:
+		set_avg_pos(cl_frame);
+		break;
+		default:
+		break;
 		}
+	}
 		else{
 
 			frame = cvQueryFrame( g_capture );
 //			if(!(++mod&0x3))
 //			cvShowImage("Camera",frame);
 #if 1
-	//		if(msPrm.box.width != 0){
-	//			draw_box(frame,msPrm.box);
+	//		if(g_msPrm.box.width != 0){
+	//			draw_box(frame,g_msPrm.box);
 	//		}
 	//		else{
 	//		}
